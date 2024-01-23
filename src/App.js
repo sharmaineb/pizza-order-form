cc77e2c47961d4f2e47a958230a680b4830c934d
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-01-23 14:13:17 -0800

    completed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+  const [name, setName] = useState("")
+  const [peperoni, setPeperoni] = useState(false)
+  const [sausage, setSausage] = useState(false)
+  const [peppers, setPeppers] = useState(false)
+  const [onions, setOnions] = useState(false)
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div children='box-1'>
+        <h1>Pizza Order</h1>
+        <label>
+          <input 
+              type="text"
+              checked={name}
+              onChange={e => setName(e.target.value)}
+            />
+        </label>
+
+        <label>
+          <input 
+            type="checkbox"
+            checked={peperoni}
+            onChange={() => setPeperoni(!peperoni)}
+          />
+          Peperoni
+        </label>
+
+        <label>
+          <input 
+              type="checkbox"
+              checked={sausage}
+              onChange={() => setSausage(!sausage)}
+            />
+            Sausage
+        </label>
+
+        <label>   
+            <input 
+              type="checkbox"
+              checked={peppers}
+              onChange={() => setPeppers(!peppers)}
+            />
+            Peppers
+        </label>
+        <label>
+          <input 
+            type="checkbox"
+            checked={onions}
+            onChange={() => setOnions(!onions)}
+          />
+          Onions
+        </label>
+        <button>Submit</button>
+      </div>
+      <div className='box-2'>
+        <h2>Your Order</h2>
+        {name && <p>{name}</p>}
+        {peperoni && <div>Pepperoni</div>}
+        {sausage && <div>Sausage</div>}
+        {peppers && <div>Peppers</div>}
+        {onions && <div>Onions</div>}
+      </div>
+
+
     </div>
   );
 }
